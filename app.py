abef890311e5e3011414333cc0854241642142b0
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-22 15:41:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 import io
-import os
-import cv2
 
 app = Flask(__name__)
 
-# Modelo de google Colab
-MODEL_PATH = "/content/drive/MyDrive/Colab Notebooks/EmotionAI.ipynb"
+# Ruta del modelo
+MODEL_PATH = "/content/drive/MyDrive/Colab Notebooks/EmotionAI.h5"  # Asegúrate de que este sea el archivo .h5
 model = load_model(MODEL_PATH)
 
 def preprocess_image(image):
-    # Convertir la imagen a escala de grises, cambiar tamaño y normalizacion
+    # Convertir la imagen a escala de grises, cambiar tamaño y normalización
     image = image.convert("L").resize((96, 96))
     image = np.array(image)
     image = image / 255.0  # Normalización
     image = np.expand_dims(image, axis=-1)
     return np.expand_dims(image, axis=0)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
